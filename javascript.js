2041fad7216ddce444d396bf91619e966490b2a6
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-03 14:10:40 +0100

    Complete

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const Nome = prompt("Ciao come ti chiami ?");
+const Cognome = prompt("Ed il tuo cognome?");
+const ColorePreferito = prompt("colore preferito??");
+
+console.log('ti chiami ' + Nome)
+console.log('Il tuo cognome è ' + Cognome)
+console.log('ed il tuo colore preferito è ' + ColorePreferito)
+
+document.getElementById('Nome').innerHTML = 'Il tuo nome è: <span>' + Nome + '</span>';
+document.getElementById('Cognome').innerHTML = 'Il tuo cognome è: <span>' + Cognome + '</span>';
+document.getElementById('ColorePreferito').innerHTML = 'Il tuo colore preferito è: <span>' + ColorePreferito + '</span>';
